97f7d675bbba174ef131d6bd352f52b3de86489b
2xburnt <169301814+2xburnt@users.noreply.github.com>
2025-03-27 13:55:27 -0500

    update preview env

--- server/api/discord.ts
+++ server/api/discord.ts
@@ -9,6 +9,7 @@ import {
     ButtonStyleTypes,
     verifyKeyMiddleware,
 } from 'discord-interactions';
+import { HttpError } from './credit';
 
 export interface FaucetRequestBody {
     readonly type: InteractionType;
@@ -17,20 +18,34 @@ export interface FaucetRequestBody {
 
 
 export default defineEventHandler(async (event) => {
-    const body: FaucetRequestBody = await event.context.cloudflare.request.json()
-    const { type, data } = body
-    console.log("Discord interaction:", type, data)
-    /**
-     * Handle slash command requests
-     */
-    if (type === InteractionType.APPLICATION_COMMAND) {
-        // Slash command with name of "faucet"
-        if (data.name === 'faucet') {
-            // Send a message as response
-            return new Response({
-                type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-                data: { content: 'Please use https://faucet.testnet.burnt.com' },
+    try {
+        const body = await readBody(event) as unknown as FaucetRequestBody;
+        const { type, data } = body
+        console.log("Discord interaction:", type, data)
+        /**
+         * Handle slash command requests
+         */
+        if (type === InteractionType.APPLICATION_COMMAND) {
+            // Slash command with name of "faucet"
+            if (data.name === 'faucet') {
+                // Send a message as response
+                return new Response(JSON.stringify({
+                    type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                    data: { content: 'Please use https://faucet.testnet.burnt.com' },
+                }), { status: 200 });
+            }
+        }
+    } catch (e) {
+        console.error(e);
+        if (e instanceof HttpError) {
+            return new Response(JSON.stringify(e), {
+                status: e.status,
+                headers: { "Content-Type": "application/json" }
             });
         }
+        return new Response(JSON.stringify(new HttpError(`Sending tokens failed: ${e}`, 500)), {
+            status: 500,
+            headers: { "Content-Type": "application/json" }
+        });
     }
 })
